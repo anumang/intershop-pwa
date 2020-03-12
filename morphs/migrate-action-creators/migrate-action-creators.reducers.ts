db83afb1d030f8cc12505dbc7229b1447b7e5453
MKless <mkless@intershop.de>
2020-03-12 13:19:06 +0100

    reducer switch statement use getStatements()

--- morphs/migrate-action-creators/migrate-action-creators.reducers.ts
+++ morphs/migrate-action-creators/migrate-action-creators.reducers.ts
@@ -8,7 +8,7 @@ export class ActionCreatorsReducerMorpher {
   switchStatements: {
     identifier: string;
     hasLogic: boolean;
-    block: string;
+    statements: string;
     previousIdentifiers: string[];
   }[];
   dependencies: string[] = [];
@@ -72,7 +72,9 @@ export class ActionCreatorsReducerMorpher {
     this.switchStatements.forEach(clause => {
       // name of the actionCreator function
       const actionTypes = createActionTypes(clause.identifier, clause.previousIdentifiers);
-      const arrowFunction = clause.hasLogic ? `(state, action) => ${clause.block}` : `state => ${clause.block}`;
+      const arrowFunction = clause.hasLogic
+        ? `(state, action) => ${clause.statements}`
+        : `state => ${clause.statements}`;
       createReducerFunction.addArgument(`on(${actionTypes}, ${arrowFunction})`);
     });
   }
@@ -128,10 +130,7 @@ export class ActionCreatorsReducerMorpher {
 
         // does the clause have a static return or is the payload used?
         const hasLogic =
-          clause
-            .getFirstChildByKindOrThrow(SyntaxKind.Block)
-            .getStatements()[0]
-            .getKind() !== SyntaxKind.ReturnStatement ||
+          clause.getStatements()[0].getKind() !== SyntaxKind.ReturnStatement ||
           clause
             .getStatements()[0]
             .getDescendants()
@@ -140,12 +139,14 @@ export class ActionCreatorsReducerMorpher {
                 desc.getKind() === SyntaxKind.PropertyAccessExpression &&
                 desc.getDescendantsOfKind(SyntaxKind.Identifier).filter(dd => dd.getText() === 'action').length > 0
             ).length > 0;
-
         // push information about switch statement to array
         this.switchStatements.push({
           identifier: clause.getExpression().getText(),
           hasLogic,
-          block: clause.getFirstChildByKindOrThrow(SyntaxKind.Block).getText(),
+          statements:
+            clause.getStatements()[0].getKind() === SyntaxKind.Block
+              ? clause.getStatements()[0].getText()
+              : `{${clause.getStatements()[0].getText()}}`,
           previousIdentifiers: [...previousIdentifiers],
         });
         previousIdentifiers = [];
