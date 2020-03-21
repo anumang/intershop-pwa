eafada13e2a60321fabfc08c729a7b719d6abe0c
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-03-21 14:41:18 +0100

    fixup! fixup! --wip-- [skip ci]

--- src/app/core/store/checkout/viewconf/viewconf.integration.spec.ts
+++ src/app/core/store/checkout/viewconf/viewconf.integration.spec.ts
@@ -1,4 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { combineReducers } from '@ngrx/store';
 
 import { checkoutReducers } from 'ish-core/store/checkout/checkout-store.module';
@@ -9,28 +12,53 @@ import { getCheckoutStep } from './viewconf.selectors';
 
 describe('Viewconf Integration', () => {
   let store$: TestStore;
+  let router: Router;
 
   beforeEach(() => {
+    @Component({ template: 'dummy' })
+    class DummyComponent {}
+
     TestBed.configureTestingModule({
-      imports: ngrxTesting({ reducers: { checkout: combineReducers(checkoutReducers) }, effects: [ViewconfEffects] }),
+      declarations: [DummyComponent],
+      imports: [
+        ...ngrxTesting({
+          reducers: { checkout: combineReducers(checkoutReducers) },
+          effects: [ViewconfEffects],
+          router: true,
+        }),
+        RouterTestingModule.withRoutes([
+          {
+            path: 'checkout',
+            children: [
+              { path: 'step1', data: { checkoutStep: 1 }, component: DummyComponent },
+              { path: 'step2', data: { checkoutStep: 2 }, component: DummyComponent },
+            ],
+          },
+          { path: '**', component: DummyComponent },
+        ]),
+      ],
     });
 
     store$ = TestBed.get(TestStore);
+    router = TestBed.get(Router);
   });
 
-  it('should extract checkoutStep from routing data to store', () => {
+  it('should extract checkoutStep from routing data to store', fakeAsync(() => {
     expect(getCheckoutStep(store$.state)).toBeUndefined();
 
-    store$.dispatch(new RouteNavigation({ path: 'checkout', data: { checkoutStep: 1 } }));
+    router.navigateByUrl('checkout/step1');
+    tick(500);
 
     expect(getCheckoutStep(store$.state)).toBe(1);
 
-    store$.dispatch(new RouteNavigation({ path: 'checkout', data: { checkoutStep: 2 } }));
+    router.navigateByUrl('checkout/step2');
+    tick(500);
 
     expect(getCheckoutStep(store$.state)).toBe(2);
 
-    store$.dispatch(new RouteNavigation({ path: 'checkout' }));
+    router.navigateByUrl('other');
+    tick(500);
 
     expect(getCheckoutStep(store$.state)).toBeUndefined();
-  });
+  }));
 });
